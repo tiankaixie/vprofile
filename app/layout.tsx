1344a1ec078131df9500cdf956d6be82048d746e
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2023-02-22 00:36:35 -0700

    resync

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
+'use client'
 import './globals.css'
 
+
+import {CacheProvider} from '@chakra-ui/next-js'
+import {ChakraProvider} from '@chakra-ui/react'
+
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode,
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='en'>
+        <head/>
+        <body>
+        <CacheProvider>
+            <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+        </body>
+        </html>
+    )
 }
